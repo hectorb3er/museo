60a4e6d78710ad5d1ec89695f7f2c46953147f8a
hectorb3er <bernalsanchezhector1cvp@cetis50cdmx.com>
2024-05-16 15:04:43 -0600

    se suben de manera simple

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-document.addEventListener('DOMContentLoaded',function(){
-    const botonBarra=document.querySelector('.Barra_Boton');
-    const botonLateral=document.querySelector('.Barra_lateral');
-    botonBarra.addEventListener('click',function(){
-        if(barraLateral.style.left==='0px'|| botonLateral.style.left===''){
-            barraLateral.style.left='-250px';
-        }else{
-            barralateral.style.left='0px';
+document.addEventListener('DOMContentLoaded', function() {
+    const botonBarra = document.querySelector('.Barra_Boton');
+    const barraLateral = document.querySelector('.barra_lateral');
+
+    botonBarra.addEventListener('click', function() {
+        // Cambiamos el valor de la propiedad left de la barra lateral para mostrar/ocultar la barra
+        if (barraLateral.style.left === '0px' || barraLateral.style.left === '') {
+            barraLateral.style.left = '-250px';
+        } else {
+            barraLateral.style.left = '0px';
         }
-
     });
-
-
 });
